61362d265e3b82399143dc4fa31f47370f7b9ee9
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-06-12 21:43:38 -0300

    AF_Comportamentos_Complexos
    Criação de 5 robos com corportamentos complexos, barra de vida e respawn do player

---
+++ GustavoHenrique_190339_AI_Final/Assets/AI_Seek.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI_Seek : MonoBehaviour
+{
+    //*criação das variaveis
+    public Transform player;
+    public Transform bulletSpawn;
+    public Slider healthBar;   
+    public GameObject bulletPrefab;
+
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+    float health = 100.0f;
+    float rotSpeed = 5.0f;
+
+    float visibleRange = 80.0f;
+    public float speed = 10;
+    //*
+
+    void Start()
+    {
+        //pega o componente navmesh do asset
+        agent = this.GetComponent<NavMeshAgent>();
+        //sempre faz o update da vida do npc
+        InvokeRepeating("UpdateHealth",5,0.5f);
+        
+    }
+
+    void Update()
+    {
+        //posiciona a barra de vida de acordo com a camera
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //seta o valor da vida como a variavel health
+        healthBar.value = (int)health;
+        //coloca a barra de vida em cima do npc
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+        //se a vida for menos que 100 recupera ela
+        if (health < 100)
+        health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //caso colide com o projetil perca 10 de vida
+        if (col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+        //caso colida em algo com a tag player
+        if (col.collider.CompareTag("Player"))
+        {
+            //pega o componente do player
+            var drive = col.collider.GetComponent<Drive>();
+            //da instakill
+            drive.Damege(100f);
+        }
+    }
+    //cria o metodo de ação da arvore de decisões de escolher uma posição aleatoria
+    [Task]
+    public void PickRandomDestination()
+    {
+        //cria a variavel que vai escolher uma posição aleatoria entre -100 e 100 nos eixo X e Z
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        //seta o destino dele para a posição escolhida aleatoriamente
+        agent.SetDestination(dest);
+        //fala que a ação foi concluida com sucesso
+        Task.current.Succeed();
+    }
+
+    //cria o metodo de ação da arvore de decisões para se mover para a posição escolhida
+    [Task]
+    public void MoveToDestination()
+    {
+        //se estiver rodando a aplicação
+        if (Task.isInspected)
+        {
+            //cira um timer para ver o tempo rodando
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        }
+        //se estiver chegado no local
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            //fala que a ação foi concluida com sucesso
+            Task.current.Succeed();
+        }
+    }
+    //cria o metodo para ir para o destino para fazer a patrulha
+    [Task]
+    public void PickDestination(int x, int z)
+    {
+        //Cria a variavel para pegar as posições dos eixos x e z
+        Vector3 dest = new Vector3(x, 0, z);
+        //Seta o destino do NPC para a variavel dest
+        agent.SetDestination(dest);
+        //fala que a ação foi concluida com sucesso
+        Task.current.Succeed();
+    }
+    [Task]
+    //metodo boolenao para olhar para o player
+    bool SeePlayer()
+    {
+        //fala que a distancia dele vai ser a distancia dele menos a do player
+        Vector3 distance = player.transform.position - this.transform.position;
+        //cria o raycast
+        RaycastHit hit;
+        //booleano para ver a parede
+        bool seeWall = false;
+        //debug para desenhar o raycast
+        Debug.DrawRay(this.transform.position, distance, Color.red);
+        //se o raycast tocar
+        if (Physics.Raycast(this.transform.position, distance, out hit))
+        {
+            //se o raycast colidir com um objeto com a tag wall seeWall fica true
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                //fica true
+                seeWall = true;
+            }
+        }
+        //se estiver rodando a aplicação
+        if (Task.isInspected)
+        {
+            //mostra a distancia da wall
+            Task.current.debugInfo = string.Format("wall={0}", seeWall);
+        }
+        //se a distancia dor menor que a distancia de visibilidade e caso seewall seja false retorne como true
+        if (distance.magnitude < visibleRange && !seeWall)
+        {
+            //retorna como true
+            return true;
+        }
+        //caso não retorne como falso
+        else
+        {
+            //retorna como false
+            return false;
+        }
+    }
+    [Task]
+    //metodo para ver o player
+    public void TargetPlayer()
+    {
+        //pega a posição do player
+        target = player.transform.position;
+        //fala que a ação foi concluida com sucesso
+        Task.current.Succeed();
+    }
+
+    [Task]
+    //olha para o target
+    public void LookAtTarget()
+    {
+        //cria a variavel para detectar a direção sendo menos que a posição do player
+        Vector3 direction = target - this.transform.position;
+        //faz a rotação ser mais suave
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        //se estiver rodando a aplicação
+        if (Task.isInspected)
+        {
+            //mostra o angle com debug
+            Task.current.debugInfo = string.Format("angle={0}", Vector3.Angle(this.transform.forward, direction));
+        }
+        //se for menor que 5 complete a task
+        if (Vector3.Angle(this.transform.forward, direction) < 5.0f)
+        {
+            //fala que a ação foi concluida com sucesso
+            Task.current.Succeed();
+        }
+    }
+    [Task]
+    //metodo booleano para atirar
+    public bool Fire()
+    {
+        //cria uma variavel para instanciar o projetil da bala
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        //da a força de movimentação para o projetil
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
+        //retorna como true
+        return true;
+    }
+    [Task]
+    //metodo booleano para olhar para o player
+    bool Turn(float angle)
+    {
+        //variavel para pegar a posição e somar com o anglo para olhar para o target
+        var p = this.transform.position + Quaternion.AngleAxis(angle, Vector3.up) * this.transform.forward;
+        //fala que o target é a variavel
+        target = p;
+        //retorna como true
+        return true;
+    }
+    [Task]
+    //metodo para verificar se a vida dele for menor que o valor escolhido
+    public bool IsHealthLessThan(float health)
+    {
+        //retorna a vida dele para o float setado no metodo
+        return this.health < health;
+    }
+    [Task]
+    //metodo para fazer o estado de morte
+    public bool Explode()
+    {
+        //destroi a barra de vida
+        Destroy(healthBar.gameObject);
+        //destroi o objeto que comtem o script
+        Destroy(this.gameObject);
+        //retorna como true
+        return true;
+    }
+    [Task]
+    //metodo para fazer o estado de perseguir
+    public void SeekPlayer()
+    {
+        //seta o destino para a posição do jogador
+        agent.SetDestination(player.position);
+        //fala que a ação foi concluida com sucesso
+        Task.current.Succeed();
+        
+    }
+}
+
---
+++ GustavoHenrique_190339_AI_Final/Assets/Drive.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Drive : MonoBehaviour
+{
+    //criação das variaveis
+    float speed = 20.0F;
+    float rotationSpeed = 120.0F;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public Slider healthBar;
+    float health = 100.0f;
+    public Transform respawn;
+
+    void Update()
+    {
+        //faz ele usar os inputs setado na vertical
+        float translation = Input.GetAxis("Vertical") * speed;
+        //faz ele usar os inputs setado na horizontal
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //faz o calculo pra ele se mover nas verticais com o deltaTime
+        translation *= Time.deltaTime;
+        //faz o calculo pra ele se mover nas horizontais com o deltaTime
+        rotation *= Time.deltaTime;
+        //fala o eixo que o translation vai interferir no caso o z
+        transform.Translate(0, 0, translation);
+        //fala o eixo que o rotation vai interferir no caso o y
+        transform.Rotate(0, rotation, 0);
+        //ao apertar a barra de espaço atira um projetiol
+        if (Input.GetKeyDown("space"))
+        {
+            //instancia o projetil
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            //da a força de tiro do projetil
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
+        }
+        //posiciona a barra de vida de acordo com a camera
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //seta o valor da vida como a variavel health
+        healthBar.value = (int)health;
+        //coloca a barra de vida em cima do npc
+        healthBar.transform.position = healthBarPos + new Vector3(0, 60, 0);
+        //caso a visa seja igual ou menor que 0
+        if (health <= 0)
+        {
+            //vida volta a ser 100
+            health = 100;
+            //coloca o player na posição do respawn
+            transform.position = respawn.position;
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        //caso colida com um projetil perca vida
+    if (col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+    }
+    //função de tomar dano
+    public void Damege(float damage)
+    {
+        //caso tome dano subitraia a vida
+        health -= damage;
+    }
+}
+
